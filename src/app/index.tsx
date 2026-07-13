d175cf1de9486d8e74b24d5e407534ce83da2bf0
hassannawaz1420@gmail.com <hassannawaz1420@gmail.com>
2026-07-13 13:49:07 +0500

    Refactor App component: simplify structure and remove unused imports

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,98 +1,30 @@
-import * as Device from 'expo-device';
-import { Platform, StyleSheet } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import {View , Text , Image, TextInput} from "react-native"
+import {useState} from "react"
 
-import { AnimatedIcon } from '@/components/animated-icon';
-import { HintRow } from '@/components/hint-row';
-import { ThemedText } from '@/components/themed-text';
-import { ThemedView } from '@/components/themed-view';
-import { WebBadge } from '@/components/web-badge';
-import { BottomTabInset, MaxContentWidth, Spacing } from '@/constants/theme';
 
-function getDevMenuHint() {
-  if (Platform.OS === 'web') {
-    return <ThemedText type="small">use browser devtools</ThemedText>;
-  }
-  if (Device.isDevice) {
-    return (
-      <ThemedText type="small">
-        shake device or press <ThemedText type="code">m</ThemedText> in terminal
-      </ThemedText>
-    );
-  }
-  const shortcut = Platform.OS === 'android' ? 'cmd+m (or ctrl+m)' : 'cmd+d';
+export default function App() {
+  const [name , setName] = useState("")
   return (
-    <ThemedText type="small">
-      press <ThemedText type="code">{shortcut}</ThemedText>
-    </ThemedText>
+    <View>
+      <Text numberOfLines={7}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit ipsam eveniet ut ducimus labore id, aliquid quisquam enim reprehenderit laboriosam exercitationem corrupti illum, facere odit excepturi fuga expedita pariatur repellat, quas est! Sed similique a suscipit dolores. Hic, a qui?quisquam enim reprehenderit laboriosam exercitationem corrupti illum, facere odit excepturi fuga expedita pariatur repellat, quas est! Sed similique a suscipit dolores. Hic, a qui</Text>
+
+
+     {/* remote image form internet */}
+      <Image
+      source={{
+        uri : "https://chaicode.com/assets/hitesh-piyush-dark-fpMoT6Mp.webp"
+      }}
+      width={300}
+      height={300}
+      blurRadius={0.3}
+      />
+      
+      <TextInput 
+      placeholder="Enter your name"
+      onChangeText={setName}
+      placeholderTextColor={"red"}
+      />
+      
+    </View>
   );
 }
-
-export default function HomeScreen() {
-  return (
-    <ThemedView style={styles.container}>
-      <SafeAreaView style={styles.safeArea}>
-        <ThemedView style={styles.heroSection}>
-          <AnimatedIcon />
-          <ThemedText type="title" style={styles.title}>
-            Welcome to&nbsp;Expo
-          </ThemedText>
-        </ThemedView>
-
-        <ThemedText type="code" style={styles.code}>
-          get started
-        </ThemedText>
-
-        <ThemedView type="backgroundElement" style={styles.stepContainer}>
-          <HintRow
-            title="Try editing"
-            hint={<ThemedText type="code">src/app/index.tsx</ThemedText>}
-          />
-          <HintRow title="Dev tools" hint={getDevMenuHint()} />
-          <HintRow
-            title="Fresh start"
-            hint={<ThemedText type="code">npm run reset-project</ThemedText>}
-          />
-        </ThemedView>
-
-        {Platform.OS === 'web' && <WebBadge />}
-      </SafeAreaView>
-    </ThemedView>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    flexDirection: 'row',
-  },
-  safeArea: {
-    flex: 1,
-    paddingHorizontal: Spacing.four,
-    alignItems: 'center',
-    gap: Spacing.three,
-    paddingBottom: BottomTabInset + Spacing.three,
-    maxWidth: MaxContentWidth,
-  },
-  heroSection: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    flex: 1,
-    paddingHorizontal: Spacing.four,
-    gap: Spacing.four,
-  },
-  title: {
-    textAlign: 'center',
-  },
-  code: {
-    textTransform: 'uppercase',
-  },
-  stepContainer: {
-    gap: Spacing.three,
-    alignSelf: 'stretch',
-    paddingHorizontal: Spacing.three,
-    paddingVertical: Spacing.four,
-    borderRadius: Spacing.four,
-  },
-});
